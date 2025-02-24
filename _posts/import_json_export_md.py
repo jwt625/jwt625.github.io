64bd1c5e99847bb69c71f1d8382a2ebc62bd7f7d
Wentao <jwt625@gmail.com>
2025-02-23 23:04:19 -0800

    ofs35

--- _posts/import_json_export_md.py
+++ _posts/import_json_export_md.py
@@ -45,8 +45,8 @@ def create_markdown(json_file, output_file):
 
 
 #%%
-json_file = "scraped_tweets_20250209_20250216.json"
-output_file = "2025-02-16-weekly-OFS-34.md"
+json_file = "scraped_tweets_20250216_20250223.json"
+output_file = "2025-02-23-weekly-OFS-35.md"
 create_markdown(json_file, output_file)
 print(f"Markdown file '{output_file}' has been created.")
 # %%
